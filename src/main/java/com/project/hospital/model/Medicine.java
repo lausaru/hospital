71aa1e79e9594f34da3afa091514dc19e1bbd0fa
lausaru <laurasanchezru@gmail.com>
2024-04-20 15:11:54 +0200

    added medicine class

---
+++ src/main/java/com/project/hospital/model/Medicine.java
@@ -0,0 +1,20 @@
+package com.project.hospital.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Medicine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+}
