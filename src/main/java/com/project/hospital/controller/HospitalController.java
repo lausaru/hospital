5d7f683fe0dc1e21883c0f785400f9937ce8c771
lausaru <laurasanchezru@gmail.com>
2024-04-20 15:07:53 +0200

    patientrepository and controller with endpoint to add new patients + corresponding test added

--- src/main/java/com/project/hospital/controller/HospitalController.java
+++ src/main/java/com/project/hospital/controller/HospitalController.java
@@ -1,2 +1,23 @@
-package com.project.hospital.controller;public class HospitalController {
+package com.project.hospital.controller;
+
+import com.project.hospital.model.Patient;
+import com.project.hospital.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HospitalController {
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @PostMapping("/patient")
+    public Patient addNewPatient(@RequestBody Patient patient) {
+        return patientRepository.save(patient);
+    }
+
 }
--- src/main/java/com/project/hospital/repository/PatientRepository.java
+++ src/main/java/com/project/hospital/repository/PatientRepository.java
@@ -1,2 +1,9 @@
-package com.project.hospital;public interface PatientRepository {
+package com.project.hospital.repository;
+
+import com.project.hospital.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient,Integer> {
 }
