0031cf4d02cd4a89557ae85b6129c7d3029265c2
lausaru <laurasanchezru@gmail.com>
2024-05-07 19:00:04 +0200

    refactor assign appointment feature - methods moved to service

--- src/main/java/com/project/hospital/model/Appointment.java
+++ src/main/java/com/project/hospital/model/Appointment.java
@@ -32,36 +32,4 @@ public class Appointment {
         setPatient(patient);
         this.diagnosis = null;
     }
-
-    public void setDoctor(Patient patient, DoctorRepository doctorRepository) throws Exception {
-        if (patient.getAppointments().isEmpty()) {
-            this.setMedGenDoctor(doctorRepository);
-        } else {
-            Diagnosis lastDiagnosis = patient.getAppointments().getLast().getDiagnosis();
-            if (lastDiagnosis.isReferred()) {
-                Specialty specialtyReferral = lastDiagnosis.getReferral().getSpecialty();
-                List<Doctor> possibleDoctors = doctorRepository.findBySpecialty(specialtyReferral.getCode());
-                if (possibleDoctors.isEmpty()) {
-                    throw new Exception("There are no doctors with the specialty to which the patient was referred to in last appointment.");
-                } else {
-                    // Get random doctor of the list
-                    Random rand = new Random();
-                    this.doctor = possibleDoctors.get(rand.nextInt(possibleDoctors.size()));
-                }
-            } else {
-                this.setMedGenDoctor(doctorRepository);
-            }
-        }
-    }
-
-    public void setMedGenDoctor(DoctorRepository doctorRepository) throws Exception {
-        List<Doctor> doctorsMedGen = doctorRepository.findBySpecialty("MG1");
-        if (doctorsMedGen.isEmpty()) {
-            throw new Exception("There are no doctors with the specialty Medicina General.");
-        } else {
-            // Get random doctor of the list
-            Random rand = new Random();
-            this.doctor = doctorsMedGen.get(rand.nextInt(doctorsMedGen.size()));
-        }
-    }
 }
--- src/main/java/com/project/hospital/service/AppointmentsService.java
+++ src/main/java/com/project/hospital/service/AppointmentsService.java
@@ -1,12 +1,13 @@
 package com.project.hospital.service;
 
-import com.project.hospital.model.Appointment;
-import com.project.hospital.model.Patient;
+import com.project.hospital.model.*;
 import com.project.hospital.repository.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 @Service
 public class AppointmentsService {
@@ -15,7 +16,41 @@ public class AppointmentsService {
 
     public void scheduleAppointment(Patient patient, Date date) throws Exception {
         Appointment newAppointment = new Appointment(date, patient);
-        newAppointment.setDoctor(patient,doctorRepository);
+        Doctor doctor = AppointmentsService.setAppointmentsDoctor(newAppointment, doctorRepository);
+        newAppointment.setDoctor(doctor);
         patient.addAppointment(newAppointment);
+    }
+
+    public static Doctor setAppointmentsDoctor(Appointment appointment, DoctorRepository doctorRepository) throws Exception {
+        Patient patient = appointment.getPatient();
+        if (patient.getAppointments().isEmpty()) {
+            return AppointmentsService.setAppointmentsMedGenDoctor(doctorRepository);
+        } else {
+            Diagnosis lastDiagnosis = patient.getAppointments().getLast().getDiagnosis();
+            if (lastDiagnosis.isReferred()) {
+                Specialty specialtyReferral = lastDiagnosis.getReferral().getSpecialty();
+                List<Doctor> possibleDoctors = doctorRepository.findBySpecialty(specialtyReferral.getCode());
+                if (possibleDoctors.isEmpty()) {
+                    throw new Exception("There are no doctors with the specialty to which the patient was referred to in last appointment.");
+                } else {
+                    // Get random doctor of the list
+                    Random rand = new Random();
+                    return possibleDoctors.get(rand.nextInt(possibleDoctors.size()));
+                }
+            } else {
+                return AppointmentsService.setAppointmentsMedGenDoctor(doctorRepository);
+            }
+        }
+    }
+
+    public static Doctor setAppointmentsMedGenDoctor(DoctorRepository doctorRepository) throws Exception {
+        List<Doctor> doctorsMedGen = doctorRepository.findBySpecialty("MG1");
+        if (doctorsMedGen.isEmpty()) {
+            throw new Exception("There are no doctors with the specialty Medicina General.");
+        } else {
+            // Get random doctor of the list
+            Random rand = new Random();
+            return doctorsMedGen.get(rand.nextInt(doctorsMedGen.size()));
+        }
     }
 }
