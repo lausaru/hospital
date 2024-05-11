8ff19072f9c7fdcb84e6dc0574877c410bd354c6
lausaru <laurasanchezru@gmail.com>
2024-05-11 13:52:46 +0200

    methods to delete doctor and patient. method to update patient info in progress

--- src/main/java/com/project/hospital/controller/PatientController.java
+++ src/main/java/com/project/hospital/controller/PatientController.java
@@ -3,12 +3,15 @@ package com.project.hospital.controller;
 import com.project.hospital.Utils;
 import com.project.hospital.model.Patient;
 import com.project.hospital.repository.PatientRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -48,5 +51,46 @@ public class PatientController {
         return ResponseEntity.ok(patientOptional.get());
     }
 
+    // Delete patient with given id
+    @DeleteMapping("/patient/{id}")
+    public ResponseEntity<?> deletePatientById(@PathVariable(name="id") String id) {
+        Optional<Patient> patientOptional = patientRepository.findById(id);
+        if (!patientOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Patient with id " + id + " not found.");
+        }
+
+        // If the patient exists, delete it from the repo
+        patientRepository.delete(patientOptional.get());
+
+        return ResponseEntity.ok("Patient"  + patientOptional.get().getFullName() + " with id " + id + " successfully deleted.");
+    }
+
+    // Modify patient data
+    @PutMapping("/patient/{id}")
+    public ResponseEntity<?> updatePatientById(@PathVariable(name="id") String id, @RequestBody Map<String, Object> updatedPatientInfo) {
+        Optional<Patient> patientOptional = patientRepository.findById(id);
+        if (!patientOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Patient with id " + id + " not found.");
+        }
+
+        Patient existingPatient = patientOptional.get();
+
+        for (Map.Entry<String, Object> entry : updatedPatientInfo.entrySet()) {
+            String propertyName = entry.getKey();
+            Object propertyValue = entry.getValue();
+
+            try {
+                Field field = Patient.class.getDeclaredField(propertyName);
+                field.setAccessible(true);
+                field.set(existingPatient, propertyValue);
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                e.printStackTrace();
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating patient.");
+            }
+        }
+        patientRepository.save(existingPatient);
+
+        return ResponseEntity.ok("Patient " + existingPatient.getFullName() + ", with id " + id + ", successfully updated.");
+    }
 
 }
